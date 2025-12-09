0bde58e566dd832d622367727c176d13c9f53e1d
Keichan15 <keisuke1515gaming@outlook.jp>
2025-12-09 22:46:55 +0900

    fix: rails db:migrate実行忘れによるschema.rbの更新漏れがあったため修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_09_113051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "parents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "line_user_id", null: false, comment: "LINEユーザーID"
+    t.string "name", limit: 100, null: false, comment: "保護者名"
+    t.datetime "updated_at", null: false
+    t.index ["line_user_id"], name: "index_parents_on_line_user_id", unique: true
+  end
 end

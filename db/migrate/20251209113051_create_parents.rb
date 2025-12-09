b6d00095cfe09249a36c0c7cfb0e3f2ea82b1af9
Keichan15 <keisuke1515gaming@outlook.jp>
2025-12-09 22:40:52 +0900

    feat: nameカラムに文字数制限を追加し、モデルにバリデーションを設定

--- db/migrate/20251209113051_create_parents.rb
+++ db/migrate/20251209113051_create_parents.rb
@@ -1,7 +1,7 @@
 class CreateParents < ActiveRecord::Migration[8.1]
   def change
     create_table :parents do |t|
-      t.string :name, null: false, comment: "保護者名"
+      t.string :name, limit: 100, null: false, comment: "保護者名"
       t.string :line_user_id, null: false, comment: "LINEユーザーID"
       t.timestamps
     end
